33fa0fabc76bf551d9165c9341d48b02529516a1
Calebe Bastos <calebehonorato@outlook.com>
2024-02-29 17:05:18 -0300

    Aualizações aula

--- Aulas/aula09.py
+++ Aulas/aula09.py
@@ -1,2 +1,11 @@
-frase = 'Curso em vídeo Python'
-print(frase[:13])
+frase = 'Curso em Vídeo Python'
+print(frase.replace('C', 'J'))
+##Basicamente sou eu brincando com as formas de manipular string no python
+print('Curso' in frase)
+print(frase.find('Curso'))
+print(frase.lower().find('vídeo'))
+print(frase.upper())
+print(frase.split())
+separado = frase.split()
+print(separado[2])
+print(separado[2][3])
