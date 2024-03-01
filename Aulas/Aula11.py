2ae65212ae6717c7167980333941bddd8b3ec3e9
Calebe Bastos <calebehonorato@outlook.com>
2024-03-01 16:37:23 -0300

    Aualizações aula e agora os exercícios

---
+++ Aulas/Aula11.py
@@ -0,0 +1,5 @@
+print('\033[1;40;45molá mundo\033[m')
+print('\033[7;30mOlá mundo\033[m')
+a = 3
+b = 5
+print('Os valores são \033[32m{} e \033[31m{}'.format(a,b))
--- Exercícios/Ex035.py
+++ Exercícios/Ex035.py
@@ -5,6 +5,6 @@ condicao1 = r1 + r2 > r3
 condicao2 = r2 + r3 > r1
 condicao3 = r1 + r3 > r2
 if condicao1 and condicao2 and condicao3 == True:
-    print('Com essas retas pode-se formar um triângulo')
+    print('\033[32mCom essas retas pode-se formar um triângulo')
 else:
-    print('Impossível fazer um triângulo com essas medidas')
+    print('\033[31mImpossível fazer um triângulo com essas medidas')
