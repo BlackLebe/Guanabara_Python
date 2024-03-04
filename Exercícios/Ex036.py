ec4d8ec7faa9e9f780f67d9064477aa1321be900
Calebe Bastos <calebehonorato@outlook.com>
2024-03-04 08:06:17 -0300

    Update oh yeah

--- Exercícios/Ex036.py
+++ Exercícios/Ex036.py
@@ -5,6 +5,8 @@ valormensal = (casa/parcela)/12
 
 if valormensal <= ((30/100)*salario):
     print('\033[32mCRÉDITO APROVADO\nDado o valor da casa ser {:.2f} e o seu salário, parcelando em {:.0f} anos, o valor mensal de cada parcela será R${:.2f}'.format(casa,parcela,valormensal))
-elif valormensal >= ((30/100)*salario):
-    print('CRÉDITO NEGADO\n')
+elif valormensal > ((30/100)*salario):
+    print('\033[31mCRÉDITO NEGADO\n')
+else:
+    print('Falta informações para o cálculo')
 
