6c72ad4ff3a04e664c23ca7aaf74d6733b3de62a
Calebe Bastos <calebehonorato@outlook.com>
2024-03-06 16:33:08 -0300

    Update oh yeah

--- Exercícios/Ex041.py
+++ Exercícios/Ex041.py
@@ -5,11 +5,11 @@ idade = date.today().year - nascimento
 
 if idade <= 9:
     print('Você é da categoria mirim')
-elif 9 > idade <= 14:
+elif idade <= 14:
     print('Você é da categoria Infantil')
-elif 14 > idade <= 19:
+elif idade <= 19:
     print('Você é Junior')
-elif 19 > idade == 20:
+elif idade == 20:
     print('Você é Sênior')
 elif idade > 20:
     print('você é master')
